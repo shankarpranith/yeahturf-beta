b244f2ac67b165f8f33a56b725f8924259479b81
Shankar Pranith <shankarpranith.ad23@bmsce.ac.in>
2025-11-22 22:35:38 +0530

    Removed unused pages and updated home page

--- index.js
+++ index.js
@@ -29,12 +29,7 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'about.html'));
-});
-app.get('/services', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'services.html'));
-});
+
 app.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'contact.html'));
 });
